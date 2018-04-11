fc4c0d1604a926c016a5b217731747bd6d4476f5
ofk <ofkjpn+github@gmail.com>
2018-04-11 11:25:37 +0900

    Fix index.jsx

--- index.jsx
+++ index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Hello } from 'react-component-quick-start';
 
 const rootElement = document.body.appendChild(document.createElement('div'));
 
 const renderApp = () => {
-  render(<p>test</p>, rootElement);
+  render(<div><Hello text="World" /></div>, rootElement);
 };
 
 renderApp();
